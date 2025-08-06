1a448202e5b8ba2a1d85daad99a2e33534da6cbb
Henry chidera <chidera9713@gmail.com>
2025-08-06 22:24:11 +0100

    feat: add daisyUI themes and update navbar links to use 'to' prop

--- app/components/nav/navbar.tsx
+++ app/components/nav/navbar.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import styles from './navbar.module.css';
-import Link from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <div className={styles.navbar}>
       <div className={styles.logo}>Privy Stars</div>
       <div className={styles.navLinks}>
-        <Link href="/" className={styles.navLink}>home</Link>
-        <Link href="/" className={styles.navLink}>about us</Link>
-        <Link href="/" className={styles.navLink}>academics</Link>
-        <Link href="/" className={styles.navLink}>admissions</Link>
-        <Link href="/" className={styles.navLink}>student life</Link>
-        <Link href="/" className={styles.navLink}>contact</Link>
+        <Link to="/" className={styles.navLink}>home</Link>
+        <Link to="/" className={styles.navLink}>about us</Link>
+        <Link to="/" className={styles.navLink}>academics</Link>
+        <Link to="/" className={styles.navLink}>admissions</Link>
+        <Link to="/" className={styles.navLink}>student life</Link>
+        <Link to="/" className={styles.navLink}>contact</Link>
       </div>
     </div>
   );
