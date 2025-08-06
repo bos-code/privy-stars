26636ce47a7a8287117de64cbfb38db2bd2644f5
Henry chidera <chidera9713@gmail.com>
2025-08-06 22:25:14 +0100

    feat: update navbar structure and styles, add Tailwind CSS configuration

--- app/components/nav/navbar.tsx
+++ app/components/nav/navbar.tsx
@@ -5,15 +5,15 @@ import {Link} from 'react-router-dom';
 export default function Navbar() {
   return (
     <div className={styles.navbar}>
-      <div className={styles.logo}>Privy Stars</div>
-      <div className={styles.navLinks}>
-        <Link to="/" className={styles.navLink}>home</Link>
-        <Link to="/" className={styles.navLink}>about us</Link>
-        <Link to="/" className={styles.navLink}>academics</Link>
-        <Link to="/" className={styles.navLink}>admissions</Link>
-        <Link to="/" className={styles.navLink}>student life</Link>
-        <Link to="/" className={styles.navLink}>contact</Link>
-      </div>
+      <div className={styles.logo}>Privy Stars Now</div>
+      <ul className={styles.navLinks}>
+      <li><Link to="/" className={styles.navLink}>home</Link></li>
+      <li><Link to="/" className={styles.navLink}>about us</Link></li>
+      <li><Link to="/" className={styles.navLink}>academics</Link></li>
+      <li><Link to="/" className={`${styles.navLink} `}>admissions</Link></li>
+      <li><Link to="/" className={styles.navLink}>student life</Link></li>
+      <li><Link to="/" className={styles.navLink}>contact</Link></li>
+      </ul>
     </div>
   );
 }
