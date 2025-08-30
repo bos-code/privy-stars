f70b7d28332802a84ba2d10247eba180af7a39b5
Henry chidera <chidera9713@gmail.com>
2025-08-30 11:15:43 +0100

    test

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,7 +2,7 @@ import Header from "~/components/header";
 import type { Route } from "./+types/home";
 import Hero from "../components/Hero";
 import Benefit from "../components/Benefit.jsx";
-import Testimonial from "~/components/Testimonial";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -16,7 +16,6 @@ export default function Home() {
     <div className="flex flex-col lg:gap-36 xl:gap-40 2xl:gap-50">
       <Hero />
       <Benefit />
-      <Testimonial />
     </div>
   );
 }
